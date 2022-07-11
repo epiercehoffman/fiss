9384b08ffdf9b4042ecba75d0b3959ccf5afc36e
Emma Pierce-Hoffman <epierceh@broadinstitute.org>
2022-07-11 15:43:45 -0400

    add logging & optional batches input

--- autosubmit.py
+++ autosubmit.py
@@ -44,30 +44,39 @@ def get_entities_submitted_for_workflow(namespace, workspace, workflow, require_
 
 
 def ready_to_submit(batch, current, previous):
+  logging.info(f"Checking if {batch} has completed {previous} successfully and has not yet been submitted for {current}...")
   current_submitted = get_entities_submitted_for_workflow(NAMESPACE, WORKSPACE, '07a-FilterBatchSites')
   previous_succeeded = get_entities_submitted_for_workflow(NAMESPACE, WORKSPACE, '06-GenerateBatchMetrics')
   ready = previous_succeeded - current_submitted
   return (batch in ready)
 
 
-def auto_submit(current, previous, interval, comment, output_log, retry=True, batches=None, dry_run=False):
+def auto_submit(current, previous, interval, comment, output_log, retry=True, batches=None, dry_run=False, batches=None):
   if batches is None:
     batches = [f"batch{i}_AoUSVPhaseI" for i in range(1,25)]
+  num_batches = len(batches)
   to_retry = []
   with open(output_log, 'a') as out:
     if retry:
       out.write("batch\ttimestamp\tworkflow\tsubmission_ok\tsubmission_response_text\n")
-    for batch in batches:
+    for i, batch in enumerate(batches):
       if ready_to_submit(batch, current, previous):
+        logging.info(f"Ready to submit {current} for {batch} (dry run = {dry_run})")
         if dry_run:
           out.write(f"{batch}\t{datetime.datetime.now()}\t{current}\tFalse\tDryRun\n")
         else:
           sub_response = fiss_api.create_submission(NAMESPACE, WORKSPACE, NAMESPACE, current, batch, 'sample_set',
                                                   delete_intermediate_outputs=True, user_comment=comment)
+          status_text = "succeeded"
+          waiting_text = ""
           if sub_response.ok:
-            time.sleep(interval)
+            if i < (num_batches - 1):
+              waiting_text = f" Waiting {interval} minutes for next submission."
+            time.sleep(interval * 60)
           else:
+            status_text = "failed"
             to_retry.append(batch)
+          logging.info(f"Submission of {current} for {batch} {status_text}." + waiting_text + "..")
           out.write(f"{batch}\t{datetime.datetime.now()}\t{current}\t{sub_response.ok}\t{sub_response.text}\n")
       else:
         to_retry.append(batch)
@@ -88,6 +97,8 @@ def main():
                       help="Submission interval, in minutes")
   parser.add_argument("-n", "--note", required=False, default=None,
                       help="Submission comment text")
+  parser.add_argument("-b", "--batches", required=False, default=None,
+                      help="Batches to try to submit (comma-separated)")
   parser.add_argument("--dry-run", required=False, default=False, action='store_true',
                       help="Dry run: don't submit anything")
   parser.add_argument("-l", "--log-level", required=False, default="INFO",
@@ -103,10 +114,11 @@ def main():
       raise ValueError('Invalid log level: %s' % log_level)
   logging.basicConfig(level=numeric_level, format='%(levelname)s: %(message)s')
 
-  auto_submit(args.current, args.previous, args.interval * 60, args.note, args.output_log, dry_run=args.dry_run)
-
-  print(help(fiss_api.create_submission))
-
+  batches = None
+  if args.batches is not None:
+    batches = args.batches.split(',')
+  auto_submit(args.current, args.previous, args.interval, args.note, args.output_log, dry_run=args.dry_run,
+    batches=batches)
 
 
 if __name__ == "__main__":
